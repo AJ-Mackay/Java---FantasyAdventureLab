d111160fe0f1d1e0589b40884fc2f9a33a82c6b6
Ruiner85 <paul.m.mackay85@gmail.com>
2020-04-02 12:16:47 +0100

    Fighter class created and tests passing

---
+++ src/main/java/Fighter.java
@@ -0,0 +1,6 @@
+public class Fighter extends Player {
+
+    public Fighter(String name, int health, int purse) {
+        super(name, health, purse);
+    }
+}
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,10 +2,14 @@ public abstract class Player {
 
     private String name;
     private int health;
+    private int purse;
+    private int maxHealth;
 
-    public Player(String name, int health){
+    public Player(String name, int health, int purse){
         this.name = name;
-        this.health = health;
+        this.health = 100;
+        this.purse = purse;
+        this.maxHealth = 100;
     }
 
     public String getName(){
@@ -14,5 +18,27 @@ public abstract class Player {
 
     public int getHealth(){
         return health;
+    }
+
+    public int getPurse(){
+        return purse;
+    }
+
+    public void setHealthReduced(int damage){
+        health -= damage;
+    }
+
+    public void healed(int healing){
+        if(health < maxHealth) {
+            health += healing;
+        }
+    }
+
+    public void increasePurse(int coins){
+        purse += coins;
+    }
+
+    public void decreasePurse(int price){
+        purse -= price;
     }
 }
